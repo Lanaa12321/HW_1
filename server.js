06ed0af4b936d22f99f25c7499e4ac041a7a9f8d
Lanaa12321 <lana.aldaeni@ucdenver.edu>
2026-02-08 23:05:13 -0700

    rendercorrection

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.post("/echo", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
