66176bcdbaed2caea477305ea0152b1225ad70b8
Lanaa12321 <lana.aldaeni@ucdenver.edu>
2026-02-08 22:47:11 -0700

    HW1_render

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.use(express.text({ type: "*/*" }));
+
+app.post("/echo", (req, res) => {
+  res.status(200).send(req.body);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
